e69b34168610edf2334d9efbb274e1ba121d7f97
Madhava1988-sha <madhavashankar.r1988@gmail.com>
2025-08-01 03:03:00 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 python -m pip install --upgrade pip
-python -m pip install --no-cache-dir -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
 python -m nltk.downloader punkt
 python -m spacy download en_core_web_sm
-echo "✅ Build script completed successfully."
